cacd365cd5b76350ae1300a09bb366bf3aec553b
francois <francois@wifi-eduroam0532.utt.fr>
2018-12-17 17:08:41 +0100

    Grille

---
+++ BrainNBack/GameController.swift
@@ -0,0 +1,37 @@
+//
+//  GameController.swift
+//  BrainNBack
+//
+//  Created by francois on 16/12/2018.
+//  Copyright © 2018 if26. All rights reserved.
+//
+
+import UIKit
+
+class GameController: UIViewController {
+
+    @IBOutlet weak var carre2: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        carre2.backgroundColor = UIColor.blue
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
